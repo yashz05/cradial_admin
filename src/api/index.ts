5a57bfb2acd018e658eb600cbbc17a73cff17bbe
Yash Chowdhari <yashc1999@gmail.com>
2024-04-26 21:41:40 +0530

    fix variat images

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,29 @@
 import { registerOverriddenValidators } from '@medusajs/medusa';
-import {IsArray, IsOptional, IsString} from 'class-validator';
+import { IsArray, IsOptional, IsString } from 'class-validator';
 import { AdminPostProductCategoriesCategoryReq as MedusaAdminPostProductCategoriesCategoryReq } from '@medusajs/medusa/dist/api/routes/admin/product-categories'
+import { AdminPostProductsProductVariantsVariantReq as MedusaAdminPostProductsProductVariantsVariantReq } from '@medusajs/medusa/dist/api/routes/admin/products/update-variant';
 
 class AdminPostProductsProductCategoriesCategoryReq extends MedusaAdminPostProductCategoriesCategoryReq {
-  
+
     @IsOptional()
     image?: string;
 
-   
+
 }
 
+
 registerOverriddenValidators(AdminPostProductsProductCategoriesCategoryReq);
+
+
+
+class AdminPostProductsProductVariantsVariantReq extends MedusaAdminPostProductsProductVariantsVariantReq {
+    @IsArray()
+    @IsOptional()
+    images?: string[];
+
+    @IsString()
+    @IsOptional()
+    thumbnail?: string;
+}
+
+registerOverriddenValidators(AdminPostProductsProductVariantsVariantReq);
