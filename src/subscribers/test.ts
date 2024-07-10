b3ef9844c7db61fe530fa02eeb77dcfc8ef31dff
Yash Chowdhari <yashc1999@gmail.com>
2024-07-11 00:05:15 +0530

    pswd reset

--- src/subscribers/test.ts
+++ src/subscribers/test.ts
@@ -3,15 +3,60 @@ import {
     type SubscriberConfig,
     type SubscriberArgs,
 } from "@medusajs/medusa"
+import nodemailer from "nodemailer";
 
-class OrderNotifierSubscriber {
-    constructor({ eventBusService }) {
-        eventBusService.subscribe("product.updated", this.handleOrder);
-    }
 
-    handleOrder = async (data) => {
-        // var product = ProductService.retrieve(data.id);
-    };
+const transporter = nodemailer.createTransport({
+    host: "smtpout.secureserver.net",
+    port: 465,
+    secure: true, // Use `true` for port 465, `false` for all other ports
+    auth: {
+        user: "contact@cradial.in",
+        pass: "MaryS@1587",
+    },
+});
+
+
+
+export default async function userPasswordResetSubscriber({
+    data, eventName, container, pluginOptions,
+}: SubscriberArgs<Record<string, string>>) {
+    console.log(data);
+    console.log("info");
+    const info = await transporter.sendMail({
+        from: '"Password Reset" <contact@cradial.in>', // sender address
+        to: data.email, // list of receivers
+        subject: "Password Reset Request", // Subject line
+        text: `Hello,
+    
+    We received a request to reset your password. If you didn't make the request, just ignore this email. Otherwise, you can reset your password using this link:
+    
+    [Reset Password Link]
+    
+    Thanks,
+    The Cradial Team`, // plain text body
+        html: `<p>Hello,</p>
+               <p>We received a request to reset your password. If you didn't make the request, just ignore this email. Otherwise, you can reset your password using this link:</p>
+               <p><a href="[Reset Password Link]">Reset Password</a></p>
+               <p>Thanks,<br>The Cradial Team</p>`, // html body
+    }).catch((err) => {
+        console.log(err);
+    });
+
+    console.log("info");
+
+    console.log("Message sent: %s", info.messageId);
+    // TODO: handle event
 }
 
-export default OrderNotifierSubscriber;
+
+
+
+export const config: SubscriberConfig = {
+    event: "user.password_reset",
+    context: {
+        subscriberId: "product-update-handler",
+    },
+};
+
+// export default userPasswordResetSubscriber;
